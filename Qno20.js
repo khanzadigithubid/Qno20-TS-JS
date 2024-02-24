ea631160e3ceb8901a9810e22d1f8e3e2a987cfb
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:50:41 +0500

    Qno20 Ts/Js and node js of 45 Assignments
    Qno20 Ts/Js and node js of 45 Assignments

---
+++ Qno20.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Think of something you could store in a array. For example, you
+ could make a list of mountains, rivers, countries, cities,
+ languages, or anything else you’d like. Write a program that
+  creates a list containing these items.*/
+let cities = ["Karachi", "Islamabad", "Lahore",
+    "Faislabad"];
+cities.map(cities => console.log(cities));
